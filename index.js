2c4e6584c7da3952681006fb5bec64bac3f96563
Krushna Mahalle <krushnamahalle056@gmail.com>
2026-06-14 22:21:54 +0530

    new code add

--- index.js
+++ index.js
@@ -5,8 +5,20 @@ const app = express();
 const PORT = 8000;
 
 // Routes
-app.get('/user',(req,res)=>{
+app.get('/api/users',(req,res)=>{
     return res.json(users);
 })
+
+
+
+app.get('/users',(req,res) => {
+    const html = `
+    <ul>
+        ${users.map((user) => `<li>${user.first_name}</li>`)}
+    </ul>
+    `
+    res.send(html);
+})
+
 
 app.listen(PORT,()=>console.log(`Server Started at PORT : ${PORT}`));

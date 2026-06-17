2d06c1af722a6187f0c3738efc36ba1b9f54861a
Krushna Mahalle <krushnamahalle056@gmail.com>
2026-06-17 22:31:30 +0530

    add changes

--- index.js
+++ index.js
@@ -28,11 +28,11 @@ app.get('/api/users',(req,res)=>{
 
 
 // // niche ka pura code different way main likha hai
-// app.get('/api/users/:id', (req,res) =>{
-//     const id = Number( req.params.id);
-//     const user = users.find((user) => user.id === id);
-//     return res.json(user);
-// });
+app.get('/api/users/:id', (req,res) =>{
+    const id = Number( req.params.id);
+    const user = users.find((user) => user.id === id);
+    return res.json(user);
+});
 
 // app.post('/api/users' , (req,res) =>{
 //     // TODO Create new user 

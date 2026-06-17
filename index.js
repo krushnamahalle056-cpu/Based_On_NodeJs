a73c5aa186d89cad85e9f5c4d551eb45c81b8fe2
Krushna Mahalle <krushnamahalle056@gmail.com>
2026-06-17 22:04:30 +0530

    new changes

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ app.get('/api/users',(req,res)=>{
     return res.json(users);
 });
 
+
 // // niche ka pura code different way main likha hai
 // app.get('/api/users/:id', (req,res) =>{
 //     const id = Number( req.params.id);
@@ -45,18 +46,28 @@ app.get('/api/users',(req,res)=>{
 // });
 
 
-app
-.route('/api/users/:id')
-.get((req,res) => {
-    const id = Number( req.params.id);
-    const user = users.find((user) => user.id === id);
-    return res.json(user);
-})
-.post((req,res)=>{
-    return res.json({Status:"pending"});
-})                 
-.patch((req,res) =>{ })   
-.delete((req,res) =>{ })   
+// app
+// .route('/api/users/:id')
+// .get((req,res) => {
+//     const id = Number( req.params.id);
+//     const user = users.find((user) => user.id === id);
+//     return res.json(user);
+// })
+// .post((req,res)=>{
+//     return res.json({Status:"pending"});
+// })                 
+// .patch((req,res) =>{ })   
+// .delete((req,res) =>{ })   
+
+
+    // Midelware - Plugin
+    app.use(express.urlencoded({extended:false}));
+
+    app.post("/api/users",(req,res)=>{
+        const body = req.body;
+        console.log("body",body);
+        return res.json({status:"success"})
+    })
 
 
 app.listen(PORT,()=>console.log(`Server Started at PORT : ${PORT}`));

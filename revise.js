4d784a2de6187d0b1abfb2e3bd29d21348470d1e
Krushna mahalle <krushnamahalle056@gmail.com>
2026-06-23 20:46:32 +0530

    creating some creasy

--- revise.js
+++ revise.js
@@ -1,1 +1,16 @@
 console.log("now I start the revision of javascript");
+
+// After long time I start the pending work about the web development 
+
+var name = prompt("Enter your name ");
+
+
+switch(name){
+    case "Krushna":{
+        if(name == "krushna"){
+            console.log("Hello, " + name + "!");
+        }
+    };
+    break;
+    
+}

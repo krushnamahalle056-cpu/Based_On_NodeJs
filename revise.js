601f7b014910be434f51e12c33c12b35caf7780c
Krushna mahalle <krushnamahalle056@gmail.com>
2026-06-25 23:04:24 +0530

    adding new code

--- revise.js
+++ revise.js
@@ -1,2 +1,12 @@
-let myName = "krushna";
+let myName = Prompt("Enter your name");
 console.log(myName.length);
+
+for(let i = 1; i<=10; i++){
+    if(myName === "krushna"){
+        console.log("my dream is become a famous person in india");
+        console.log("My name is :"+myName);
+    }else{
+        Alert("my name is not krushna");
+    }
+
+}

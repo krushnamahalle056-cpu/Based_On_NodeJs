e43e427daeed328ece9ed8a3607d00060364f3b0
Krushna mahalle <krushnamahalle056@gmail.com>
2026-06-25 22:55:09 +0530

    new code

--- revise.js
+++ revise.js
@@ -1,20 +1,2 @@
-console.log("Now I start the revision of JavaScript");
-
-// After a long time I start the pending work about web development
-
-switch ('Kashish') {
-    case "Krushna":
-        console.log("Hello Krushna");
-        break;
-
-    case "Rohan":
-        console.log("Hello Rohan");
-        break;
-
-    case "Kashish":
-        console.log(" Kashish is a BestFriendForever of Krushna");
-        break;
-
-    default:
-        console.log("Enter correct name");
-}
+let myName = "krushna";
+console.log(myName.length);

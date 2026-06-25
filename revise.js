64368ac92cb935730afd4a501f9f1e15d4fc06b8
Krushna mahalle <krushnamahalle056@gmail.com>
2026-06-25 23:36:52 +0530

    array concept

--- revise.js
+++ revise.js
@@ -19,3 +19,12 @@ console.log(myName.toUpperCase().trim());
 console.log(myName.toUpperCase().trim().slice(0,3));
 console.log(myName.toUpperCase().trim().slice(0,2).concat("ish").toUpperCase());
 
+
+let table = [2,4,6,8,10,12,14,16,18,20];
+console.log(table);
+
+// array methods revision
+console.log(table.length);
+console.log(table[0]);
+console.log(table.push(22));
+console.log(table.unshift(0));

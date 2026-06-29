ff4cb92af7646fdbcd6fc3fee9d53b49ea99b554
Krushna Mahalle <krushnamahalle056@gmail.com>
2026-06-30 00:13:23 +0530

    clean code

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
 const express = require("express");
-const fs = require("fs");
-const mongoose = require("mongoose");
-
 const {connectMongoDb} = require("./Connection");
+
+const {logReqRes} = require("./Middleware/index");
 
 const userRouter = require("./routes/user");
 
-const {logReqRes} = require("./Middleware/index");
 const app = express();
 const PORT = 8000;
 
 
 // // Connection  -- Mongoose ka hoga
-
 connectMongoDb("mongodb://127.0.0.1:27017/youtube-kru-1")
 
 // Midelware - Plugin
@@ -21,9 +18,8 @@ app.use(logReqRes("log.txt"));
 
 
 // //Routes
-
 app.use("./user", userRouter);
 
- 
+
 app.listen(PORT,()=>console.log(`Server Started at PORT : ${PORT}`));
 

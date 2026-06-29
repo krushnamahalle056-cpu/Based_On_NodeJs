155ee535e1e4c8141faa5788c1f9019aa2155ada
Krushna Mahalle <krushnamahalle056@gmail.com>
2026-06-30 01:12:25 +0530

    add changes

--- controllers/user.js
+++ controllers/user.js
@@ -1,12 +1,21 @@
 const User = require("../models/user");   // '../' means ek directory piche jana
 
-async function handelGetAllUsers(req, res){
+async function handleGetAllUsers(req, res){
     const allDbUsers = await User.find ({}); 
     return res.json(allDbUsers);
+}
+
+async function handleGetUserById(req, res){
+    const user = await User.findById(req.params.id);
+    if(!user){
+        return res.status(404).json({error: "User not found"});
+    }
+    return res.json(user);
 }
 
 
 module.exports = {
     handelGetAllUsers,
+    getUserById,
 
 }

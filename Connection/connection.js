08f24b10243bd4682815d4b2a6949b0a6d2d9cec
Krushna Mahalle <krushnamahalle056@gmail.com>
2026-07-01 20:48:10 +0530

    connection comment

--- Connection/connection.js
+++ Connection/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-async function connectMongoDb(url){
+async function connectMongoDb(url){        // url mongodb se liya tha  terminal se
     return mongoose.connect(url)
 }
 

050b3e872b223806106fd716f1d448e13b166de8
Krushna Mahalle <krushnamahalle056@gmail.com>
2026-07-01 22:45:43 +0530

    change roues

--- SHORT-URL-PROJECT/routes/url.js
+++ SHORT-URL-PROJECT/routes/url.js
@@ -1,7 +1,7 @@
 const express = require('express');
-
+const { handleGenerateNewShortURL } = require('../controllers/url');
 const router = express.Router();
 
-router.post('/',(req,res)=>{
-    
-})
+router.post('/', handleGenerateNewShortURL);
+
+module.exports = router;

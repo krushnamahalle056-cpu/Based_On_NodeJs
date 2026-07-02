c2444935971811857ff13c9653a39c7ec6883076
Krushna Mahalle <krushnamahalle056@gmail.com>
2026-07-02 22:31:26 +0530

    add changes

--- SHORT-URL-PROJECT/index.js
+++ SHORT-URL-PROJECT/index.js
@@ -1,7 +1,15 @@
 const express = require("express");
+const { connnectToMongoDB } = require("./connect");
+const URLRoutes = require("./routes/url");
 
 const app = express();
 const PORT = 8001;
 
-app.listen(PORT , () => console.log(`Server Started at PORT =${PORT}`))
+connnectToMongoDB("mongodb://localhost:27017/short-url").then(() => {
+    console.log("MongoDB connected");
+});
+
+app.use("/url", URLRoutes);
+
+app.listen(PORT, () => console.log(`Server Started at PORT =${PORT}`))
 

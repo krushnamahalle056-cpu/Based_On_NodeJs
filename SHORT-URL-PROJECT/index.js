e9fa806f0c29abbd05b467faca91529452a62de8
Krushna Mahalle <krushnamahalle056@gmail.com>
2026-07-03 22:18:47 +0530

    add code

--- SHORT-URL-PROJECT/index.js
+++ SHORT-URL-PROJECT/index.js
@@ -16,7 +16,7 @@ app.use("/url", URLRoutes);
 
 app.get("/:shortId", async(req, res) =>{
     const shortId = req.params.shortId;
-  const entry = await URL.findOneAndUpdate({shortId: shortId}, {$push: {visitHistory: {timestamp: Date.now()}}});
+  const entry = await URL.findOneAndUpdate({shortId}, {$push: {visitHistory: {timestamp: Date.now()}}});
   res.redirect(entry.redirectURL);
 });
 

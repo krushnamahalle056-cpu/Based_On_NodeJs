5648175bf63abd7d385354fec2dd523a938956a8
Krushna Mahalle <krushnamahalle056@gmail.com>
2026-07-03 22:36:54 +0530

    add get analytics

--- SHORT-URL-PROJECT/controllers/url.js
+++ SHORT-URL-PROJECT/controllers/url.js
@@ -20,7 +20,10 @@ async function handleGenerateNewShortURL(req, res){
 async function handleGetAnalytics(req, res){
     const shortId = req.params.shortId;
     const result = await URL.findOne({shortId});
-    return res.json(result);
+    return res.json({
+        totalClicks: result.visitHistory.length,
+        analytics: result.visitHistory,
+    })
 }
 module.exports = {
     handleGenerateNewShortURL,

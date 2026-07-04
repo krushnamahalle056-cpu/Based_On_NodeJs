f2cabb9c00b58729f70e78da7ca714f562e4cfe0
Krushna Mahalle <krushnamahalle056@gmail.com>
2026-07-04 23:06:12 +0530

    string revise

--- revise.js
+++ revise.js
@@ -14,42 +14,42 @@
 
 // }
 
-// // String methods revision 
+// String methods revision 
 
-// console.log(myName.trim());
-// console.log(myName.toLowerCase());
-// console.log(myName.toUpperCase().trim());
-// console.log(myName.toUpperCase().trim().slice(0,3));
-// console.log(myName.toUpperCase().trim().slice(0,2).concat("ish").toUpperCase());
+console.log(myName.trim());
+console.log(myName.toLowerCase());
+console.log(myName.toUpperCase().trim());
+console.log(myName.toUpperCase().trim().slice(0,3));
+console.log(myName.toUpperCase().trim().slice(0,2).concat("ish").toUpperCase());
 
 
-// let table = [2,4,6,8,10,12,14,16,18,20];
-// console.log(table);
+let table = [2,4,6,8,10,12,14,16,18,20];
+console.log(table);
 
-// // array methods revision
-// console.log(table.length);
-// console.log(table[0]);
-// console.log(table.push(22));
-// console.log(table.shift());
+// array methods revision
+console.log(table.length);
+console.log(table[0]);
+console.log(table.push(22));
+console.log(table.shift());
 
-// console.log(table);
+console.log(table);
 
-// console.log(table.pop(22));
-// console.log(table.unshift(2))
+console.log(table.pop(22));
+console.log(table.unshift(2))
 
-// console.log(table);
+console.log(table);
 
-// console.log(table.includes(8));
+console.log(table.includes(8));
 
-// let boys = ["krushna","sarthak"];
-// let girls = ["Aishwarya","Kashish"];
-// console.log(boys.concat(girls));
+let boys = ["krushna","sarthak"];
+let girls = ["Aishwarya","Kashish"];
+console.log(boys.concat(girls));
 
-// console.log(boys.join("girls"));
-// console.log(boys.reverse());
-// console.log(boys.slice());
-// console.log(girls.splice(0,1));
-// console.log(girls);
+console.log(boys.join("girls"));
+console.log(boys.reverse());
+console.log(boys.slice());
+console.log(girls.splice(0,1));
+console.log(girls);
 
 let students = ["krushna","sarthak","Hariom", "pavan","Aishwarya","kashish","shreya","vishal"];
 console.log(students.sort());

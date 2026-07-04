d441f833a78f575de96ffeb7730d049468e3b071
Krushna Mahalle <krushnamahalle056@gmail.com>
2026-07-04 23:06:59 +0530

    work end

--- revise.js
+++ revise.js
@@ -1,18 +1,18 @@
-// // let name = prompt("Enter your name");
-// // console.log(name);
+let name = prompt("Enter your name");
+console.log(name);
 
-// let myName = "   Krushna    ";
-// console.log(myName.length);
+let myName = "   Krushna    ";
+console.log(myName.length);
 
-// for(let i = 1; i<=10; i++){
-//     if(myName === "krushna"){
-//         console.log("my dream is become a famous person in india");
-//         console.log("My name is :"+myName);
-//     }else{
-//         console.log("my name is not krushna");
-//     }
+for(let i = 1; i<=10; i++){
+    if(myName === "krushna"){
+        console.log("my dream is become a famous person in india");
+        console.log("My name is :"+myName);
+    }else{
+        console.log("my name is not krushna");
+    }
 
-// }
+}
 
 // String methods revision 
 

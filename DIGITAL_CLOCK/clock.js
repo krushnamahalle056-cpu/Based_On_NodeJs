d4c5508ec9d14066edbdf110f980f9ab3b3dc61a
Krushna Mahalle <krushnamahalle056@gmail.com>
2026-07-06 23:54:49 +0530

    add changes

--- DIGITAL_CLOCK/clock.js
+++ DIGITAL_CLOCK/clock.js
@@ -3,5 +3,5 @@ function updateClock(){
     let hours = now.getHours().toString().padStart(2, '0');
     let minutes = now.getMinutes().toString().padStart(2, '0');
     let seconds = now.getSeconds().toString().padStart(2, '0');
-    document.getElementById('clock').textContent = `${hours}:${minutes}:${seconds}`;
+    document.getElementById('clock').innerHTML = `${hours}:${minutes}:${seconds}`;
 }

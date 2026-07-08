d1f83e82e76c2ade6f5fff3af39b78c7dc8ad285
Krushna Mahalle <krushnamahalle056@gmail.com>
2026-07-08 23:50:13 +0530

    init days and months

--- DIGITAL_CLOCK/clock.js
+++ DIGITAL_CLOCK/clock.js
@@ -6,6 +6,9 @@ function updateClock(){
     let s = String(now.getSeconds()).padStart(2,"0");
 
     document.getElementById("clock").innerHTML = `${h}:${m}:${s}`;
+    
+    let days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    let months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
 
 
 }

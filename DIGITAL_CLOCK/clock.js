4f754b1d0b1f6814c5b415cd5d5a999d91c34678
Krushna Mahalle <krushnamahalle056@gmail.com>
2026-07-08 23:51:56 +0530

    add DMO

--- DIGITAL_CLOCK/clock.js
+++ DIGITAL_CLOCK/clock.js
@@ -6,10 +6,14 @@ function updateClock(){
     let s = String(now.getSeconds()).padStart(2,"0");
 
     document.getElementById("clock").innerHTML = `${h}:${m}:${s}`;
-    
+
     let days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
     let months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
 
+    document.getElementById("date").innerHTML =
+    `${days[now.getDay()]}, ${now.getDate()} ${months[now.getMonth()]} ${now.getFullYear()}`;
+
+    let greeting = "Good Evening";
 
 }
 

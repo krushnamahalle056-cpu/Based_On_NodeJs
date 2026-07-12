b02d2d5b77478778196cda282991206a13971dee
Krushna Mahalle <krushnamahalle056@gmail.com>
2026-07-12 23:17:45 +0530

    func themeBtn

--- DIGITAL_CLOCK/clock.js
+++ DIGITAL_CLOCK/clock.js
@@ -61,3 +61,14 @@ updateClock();
 
 const themeBtn = document.getElementById("themeBtn");
 
+themeBtn.addEventListener("click", () => {
+
+    document.body.classList.toggle("light");
+
+    if(document.body.classList.contains("light")){
+        themeBtn.innerHTML = '<i class="fa-solid fa-sun"></i>';
+    }else{
+        themeBtn.innerHTML = '<i class="fa-solid fa-moon"></i>';
+    }
+
+});

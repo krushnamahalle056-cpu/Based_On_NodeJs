bdc243d343e2d3f54a1f4ccf189443bd4127a756
Krushna Mahalle <krushnamahalle056@gmail.com>
2026-07-13 22:26:49 +0530

    add changes

--- DIGITAL_CLOCK/clock.js
+++ DIGITAL_CLOCK/clock.js
@@ -23,7 +23,7 @@ let m = String(now.getMinutes()).padStart(2,"0");
 let s = String(now.getSeconds()).padStart(2,"0");
 
 document.getElementById("clock").innerHTML =
-`${h}:${m}:${s}`;
+`${h}:${m}:${s}${ampm}`;
 
 const days=[
 "Sunday",

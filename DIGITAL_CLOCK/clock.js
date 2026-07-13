8518aceea37f9a4de55f57f456f6c68abbd68194
Krushna Mahalle <krushnamahalle056@gmail.com>
2026-07-13 22:44:31 +0530

    add changes

--- DIGITAL_CLOCK/clock.js
+++ DIGITAL_CLOCK/clock.js
@@ -9,7 +9,17 @@ formatBtn.addEventListener("click", () => {
 });
 
 function updateClock(){
+const currentHour = now.getHours();
 
+let h = currentHour;
+let ampm = "";
+
+if(!is24Hour){
+    ampm = h >= 12 ? " PM" : " AM";
+    h = h % 12 || 12;
+}
+
+h = String(h).padStart(2,"0");
 let m = String(now.getMinutes()).padStart(2,"0");
 let s = String(now.getSeconds()).padStart(2,"0");
 

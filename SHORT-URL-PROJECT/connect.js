7d0df1aaa73135a1c9568f59317cc1722ab42916
Krushna Mahalle <krushnamahalle056@gmail.com>
2026-07-16 23:10:38 +0530

    add note

--- SHORT-URL-PROJECT/connect.js
+++ SHORT-URL-PROJECT/connect.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 async function connnectToMongoDB(url) {
-    return mongoose.connect(url);
+    return mongoose.connect(url);             // MongoDB ko connect karne ke liye mongoose ka url pass krna hoga index.js file main 
 }
 
 module.exports = {

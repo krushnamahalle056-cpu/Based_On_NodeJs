059a83c0349a205bf78a01248a73f212e8c2e62e
Krushna Mahalle <krushnamahalle056@gmail.com>
2026-07-20 23:42:19 +0530

    create user func add

--- SHORT-URL-PROJECT/controllers/user.js
+++ SHORT-URL-PROJECT/controllers/user.js
@@ -3,11 +3,19 @@ const User = require('../models/user');
 async function handleUserSignup(req, res) {
     const { name, email, password } = req.body;
 
-    try {
-        const user = new User({ name, email, password });
-        await user.save();
-        res.status(201).json({ message: 'User created successfully' });
-    } catch (error) {
-        res.status(400).json({ error: error.message });
-    }
+    await User.create({
+        name,
+        email,
+        password,
+    });
+
+    return res.render("home");   
+
+    // try {
+    //     const user = new User({ name, email, password });
+    //     await user.save();
+    //     res.status(201).json({ message: 'User created successfully' });
+    // } catch (error) {
+    //     res.status(400).json({ error: error.message });
+    // }
 }

d13c07f1fa137a081127cbf5fc58afd9315ecdc4
Krushna Mahalle <krushnamahalle056@gmail.com>
2026-07-20 23:36:18 +0530

    add handelUser func

---
+++ SHORT-URL-PROJECT/controllers/user.js
@@ -0,0 +1,13 @@
+
+
+async function handleUserSignup(req, res) {
+    const { name, email, password } = req.body;
+
+    try {
+        const user = new User({ name, email, password });
+        await user.save();
+        res.status(201).json({ message: 'User created successfully' });
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+}

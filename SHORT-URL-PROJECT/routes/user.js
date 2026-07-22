f551e8ca1b7af80a3557605b531c50e63f894d4c
Krushna Mahalle <krushnamahalle056@gmail.com>
2026-07-22 23:58:00 +0530

    new route

--- SHORT-URL-PROJECT/routes/user.js
+++ SHORT-URL-PROJECT/routes/user.js
@@ -3,6 +3,8 @@ const { handleUserSignup } = require("../controllers/user");
 
 const router = express.Router();
 
-router.post("/",handleUserSignup)
+router.post("/",handleUserSignup);
+
+router.post("/login",handleUserSignup);
 
 module.exports = router;

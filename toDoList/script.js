7ed937e56dcfbae2efdde05d83c26198a55c3f7e
Krushna Mahalle <krushnamahalle056@gmail.com>
2026-08-05 22:02:36 +0530

    TO DO LIST

--- toDoList/script.js
+++ toDoList/script.js
@@ -1,13 +1,13 @@
 function addTask() {
     let input = document.getElementById("taskInput");
+    
     let task = input.value.trim();
 
     if (task === "") {
         alert("Please enter a task.");
         return;
     }
-
-    
+   
     let li = document.createElement("li");
     li.innerHTML = `
         ${task}

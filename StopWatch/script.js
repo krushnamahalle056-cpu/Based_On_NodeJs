2510fe8f6eb88861579d8c7947c97443b2c7afa4
Krushna Mahalle <krushnamahalle056@gmail.com>
2026-08-07 22:03:35 +0530

    Implement StopWatch Timer Logic

---
+++ StopWatch/script.js
@@ -0,0 +1,52 @@
+let seconds = 0;
+let minutes = 0;
+let hours = 0;
+let timer = null;
+
+const display = document.getElementById("display");
+
+function updateDisplay() {
+  let h = String(hours).padStart(2, "0");
+  let m = String(minutes).padStart(2, "0");
+  let s = String(seconds).padStart(2, "0");
+
+  display.textContent = `${h}:${m}:${s}`;
+}
+
+function startWatch() {
+  if (timer !== null) return;
+
+  timer = setInterval(() => {
+    seconds++;
+
+    if (seconds === 60) {
+      seconds = 0;
+      minutes++;
+    }
+
+    if (minutes === 60) {
+      minutes = 0;
+      hours++;
+    }
+
+    updateDisplay();
+  }, 1000);
+}
+
+function pauseWatch() {
+  clearInterval(timer);
+  timer = null;
+}
+
+
+function resetWatch() {
+  pauseWatch();
+  seconds = 0;
+  minutes = 0;
+  hours = 0;
+  updateDisplay();
+}
+
+document.getElementById("start").addEventListener("click", startWatch);
+document.getElementById("pause").addEventListener("click", pauseWatch);
+document.getElementById("reset").addEventListener("click", resetWatch);

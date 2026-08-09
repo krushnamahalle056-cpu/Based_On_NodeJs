6bd4301332bf1650026c61b691c95eb48cfe15be
Krushna Mahalle <krushnamahalle056@gmail.com>
2026-08-09 20:57:04 +0530

    add clear all tasks functionality

--- TodoApp/script.js
+++ TodoApp/script.js
@@ -43,7 +43,7 @@ function renderTasks() {
             renderTasks();
         });
 
-        
+
         // Delete task
         li.querySelector(".delete-btn").addEventListener("click", () => {
             tasks = tasks.filter(t => t.id !== task.id);
@@ -51,6 +51,7 @@ function renderTasks() {
             renderTasks();
         });
 
+        
         taskList.appendChild(li);
     });
 

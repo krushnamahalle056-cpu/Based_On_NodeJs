58884ec39fab792896684fc67700221182a45820
Krushna Mahalle <krushnamahalle056@gmail.com>
2026-08-17 22:40:58 +0530

    display current time

---
+++ DigitalClock/script.js
@@ -0,0 +1,19 @@
+function showTime() {
+  let now = new Date();
+
+  let hours = now.getHours();
+  let minutes = now.getMinutes();
+  let seconds = now.getSeconds();
+
+  
+  hours = hours < 10 ? "0" + hours : hours;
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  seconds = seconds < 10 ? "0" + seconds : seconds;
+
+  document.getElementById("time").innerText =
+    hours + ":" + minutes + ":" + seconds;
+}
+
+setInterval(showTime, 1000);
+
+showTime();

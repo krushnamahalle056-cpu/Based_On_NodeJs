facd68bfd287a8a3434a4b5727318184bb6fa026
Krushna Mahalle <krushnamahalle056@gmail.com>
2026-08-18 23:06:36 +0530

    add temperature conversion function

---
+++ TempConverter/script.js
@@ -0,0 +1,22 @@
+function convertTemperature() {
+
+    let temperature = Number(
+        document.getElementById("temperature").value
+    );
+
+    let unit = document.getElementById("unit").value;
+    let result;
+
+    if (unit === "celsius") {
+        result = (temperature * 9 / 5) + 32;
+
+        document.getElementById("result").textContent =
+            "Result: " + result.toFixed(2) + " °F";
+
+    } else {
+        result = (temperature - 32) * 5 / 9;
+
+        document.getElementById("result").textContent =
+            "Result: " + result.toFixed(2) + " °C";
+    }
+}

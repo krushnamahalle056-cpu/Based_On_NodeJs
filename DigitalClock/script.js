2fa01a99d66d9cff072e168d50c7d20fcc3cb1cb
Krushna Mahalle <krushnamahalle056@gmail.com>
2026-08-19 22:38:18 +0530

    Integrate generated color with preview

--- DigitalClock/script.js
+++ DigitalClock/script.js
@@ -5,7 +5,6 @@ function showTime() {
   let minutes = now.getMinutes();
   let seconds = now.getSeconds();
 
-
   hours = hours < 10 ? "0" + hours : hours;
   minutes = minutes < 10 ? "0" + minutes : minutes;
   seconds = seconds < 10 ? "0" + seconds : seconds;

a9d9b690180ab80168c3cba40f1e1d294a28dace
Krushna Mahalle <krushnamahalle056@gmail.com>
2026-08-19 22:41:31 +0530

    generated HEX color code

--- ColourGenerator/script.js
+++ ColourGenerator/script.js
@@ -1,8 +1,8 @@
 function changeColor() {
   const letters = "0123456789ABCDEF";
-  let color = "#";
+  let color = " # ";
 
-  
+
   for (let i = 0; i < 6; i++) {
     color += letters[Math.floor(Math.random() * 16)];
   }

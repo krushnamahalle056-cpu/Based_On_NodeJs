b7d06c5dfcf13a097982e00afc9faad0f48124ac
Krushna Mahalle <krushnamahalle056@gmail.com>
2026-08-19 22:40:52 +0530

    random HEX color generation

---
+++ ColourGenerator/script.js
@@ -0,0 +1,12 @@
+function changeColor() {
+  const letters = "0123456789ABCDEF";
+  let color = "#";
+
+  
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+
+  document.getElementById("colorBox").style.backgroundColor = color;
+  document.getElementById("colorCode").textContent = color;
+}
